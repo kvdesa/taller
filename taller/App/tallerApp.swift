cd20b5cc95cfc473cc796d3a6039d671fd95c255
Kévin <kvdesa@gmail.com>
2026-03-23 15:50:51 -0300

    chore: structuring project

---
+++ taller/App/tallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tallerApp.swift
+//  taller
+//
+//  Created by Kevin on 23.03.26.
+//
+
+import SwiftUI
+
+@main
+struct tallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tallerTests/tallerTests.swift
@@ -0,0 +1,17 @@
+//
+//  tallerTests.swift
+//  tallerTests
+//
+//  Created by Kevin on 23.03.26.
+//
+
+import Testing
+@testable import taller
+
+struct tallerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
